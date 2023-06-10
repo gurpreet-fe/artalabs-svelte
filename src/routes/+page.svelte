76ada729036d672ce9da2dfaad4e394c909e6399
Gurpreet Singh <gur.singh2022@gmail.com>
2023-06-10 22:57:31 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,73 @@
-<h1 class="bg-sky-700 px-4 py-2">Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="pt-2">
+	<div class="container">
+		<h1 class="border-y text-center text-5xl md:text-9xl">ARTA LABS</h1>
+	</div>
+</div>
+
+<div class="flex border-b">
+	<section class="p-3">
+		<div>About</div>
+		<div>
+			Arta Labs are a freelance Web Development & UI/UX Design lab based in the UK, crafting bespoke
+			and functional experiences for clients worldwide.
+		</div>
+	</section>
+	<aside class="ml-auto grid min-w-7 place-content-center border-l">
+		<div class="vertical-lr flex rotate-180 items-center gap-2">
+			Services
+			<i class="fa-solid fa-arrow-right" />
+		</div>
+	</aside>
+</div>
+
+<div class="flex border-b">
+	<section class="p-3">
+		<div>Our Work</div>
+	</section>
+	<aside class="ml-auto grid min-w-7 place-content-center border-l">
+		<div class="vertical-lr flex rotate-180 items-center gap-2">
+			Services
+			<i class="fa-solid fa-arrow-right" />
+		</div>
+	</aside>
+</div>
+
+<div class="flex">
+	<section class="p-3">
+		<div>Contact</div>
+		<div>Get in touch for a free quote!</div>
+		<div>
+			Let us know about yourself and what you need so we can help recommend how best to help you
+		</div>
+	</section>
+	<aside class="ml-auto grid min-w-7 place-content-center border-l">
+		<i class="fa-brands fa-instagram" />
+	</aside>
+</div>
+
+<div class="flex">
+	<div class="flex-grow bg-black p-3 text-white">
+		<form action="" class="flex flex-col gap-5">
+			<div class="flex flex-col gap-1">
+				<label for=""> Name </label>
+				<input type="text" />
+			</div>
+			<div class="flex flex-col gap-1">
+				<label for=""> E-Mail </label>
+				<input type="text" />
+			</div>
+			<div class="flex flex-col gap-1">
+				<label for=""> Reason/Service </label>
+				<input type="text" />
+			</div>
+			<div class="flex flex-col gap-1">
+				<label for=""> Tell us about what you need (scope, timeline, budget): </label>
+				<input type="text" />
+			</div>
+			<input type="submit" value="" />
+		</form>
+	</div>
+	<aside class="ml-auto grid min-w-7 place-content-center border-l">
+		<div class="vertical-lr flex rotate-180 items-center gap-2">contact.artalabs@gmail.com</div>
+	</aside>
+</div>
